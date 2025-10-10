d87948f71919e7ca3f44270d5557eacd79b2d567
Simon Da Vall <simon.davall@gmail.com>
2025-10-10 16:44:56 +0100

    Added Advent of Code console application template

---
+++ AocTemplate/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics;
+
+namespace MyProject;
+
+internal static class Program
+{
+  private const long ExpectedPartOne = 0;
+  private const long ExpectedPartTwo = 0;
+
+  public static int Main(string[] args)
+  {
+    PrintTitle();
+    var input = GetData(args);
+    var stopwatch = Stopwatch.StartNew();
+
+    var resultPartOne = PartOne(input);
+    PrintResult("1", resultPartOne.ToString(), stopwatch);
+
+    var resultPartTwo = PartTwo();
+    PrintResult("2", resultPartTwo.ToString(), stopwatch);
+
+    return resultPartOne == ExpectedPartOne && resultPartTwo == ExpectedPartTwo ? 0 : 1;
+  }
+
+  private static long PartOne(string[] modules)
+  {
+    long tally = 0;
+
+    return tally;
+  }
+
+  private static long PartTwo()
+  {
+    long tally = 0;
+
+    return tally;
+  }
+
+  private static string[] GetData(string[] args)
+  {
+    var filename = "sample.txt";
+    if (args.Length > 0 && !string.IsNullOrWhiteSpace(args[0]))
+      filename = args[0];
+
+    using var streamReader = new StreamReader(filename);
+    var data = streamReader.ReadToEnd().Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+    return data;
+  }
+
+
+  private static void PrintTitle(){
+    Console.WriteLine("# Advent of Code 2019 #");
+    Console.WriteLine("## Day 1: The Tyranny of the Rocket Equation ##");
+    Console.WriteLine("https://adventofcode.com/2019/day/1");
+    Console.WriteLine();
+  }
+
+  private static void PrintResult(string partNo, string result, Stopwatch sw)
+  {
+    sw.Stop();
+    Console.WriteLine($"Part {partNo}");
+    Console.WriteLine($"Result: {result}");
+    Console.WriteLine($"Time elapsed (ms): {sw.Elapsed.TotalMilliseconds}");
+    Console.WriteLine();
+    sw.Restart();
+  }
+}
---
+++ AocTemplate/readme.md
@@ -0,0 +1,12 @@
+# Advent of Code 2019 #
+## Day 1: The Tyranny of the Rocket Equation ##
+https://adventofcode.com/2019/day/1
+
+Part 1
+Result: 3226822
+Time elapsed (ms): 3.9697
+
+Part 2
+Result: 0
+Time elapsed (ms): 0.0712
+
