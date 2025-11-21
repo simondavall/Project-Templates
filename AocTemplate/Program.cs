da2c7d9c06273d446321768b40a74545def5281b
Simon Da Vall <simon.davall@gmail.com>
2025-11-21 09:29:52 +0000

    Restructured template, separating boilerplate from solution

--- AocTemplate/Program.cs
+++ AocTemplate/Program.cs
@@ -1,68 +1,49 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 
 namespace MyProject;
 
-internal static class Program
-{
+internal static partial class Program {
+  private const string Title = "\n## Day 0: Replace with days title ##";
+  private const string AdventOfCode = "https://adventofcode.com/2019/day/0";
   private const long ExpectedPartOne = 0;
   private const long ExpectedPartTwo = 0;
 
-  public static int Main(string[] args)
-  {
-    PrintTitle();
-    var input = GetData(args);
-    var stopwatch = Stopwatch.StartNew();
+  public static int Main(string[] args) {
+    Console.WriteLine(Title);
+    Console.WriteLine(AdventOfCode);
 
-    var resultPartOne = PartOne(input);
-    PrintResult("1", resultPartOne.ToString(), stopwatch);
+    long resultPartOne = -1;
+    long resultPartTwo = -1;
 
-    var resultPartTwo = PartTwo();
-    PrintResult("2", resultPartTwo.ToString(), stopwatch);
+    foreach (var filePath in args) {
+      Console.WriteLine($"\nFile: {filePath}\n");
+      string[] input = GetData(filePath);
+      var stopwatch = Stopwatch.StartNew();
+
+      resultPartOne = PartOne(input);
+      PrintResult("1", resultPartOne.ToString(), stopwatch);
+
+      resultPartTwo = PartTwo(input);
+      PrintResult("2", resultPartTwo.ToString(), stopwatch);
+    }
 
     return resultPartOne == ExpectedPartOne && resultPartTwo == ExpectedPartTwo ? 0 : 1;
   }
 
-  private static long PartOne(string[] modules)
-  {
-    long tally = 0;
+  private static string[] GetData(string filePath) {
+    if (string.IsNullOrWhiteSpace(filePath)) {
+      filePath = "sample.txt";
+    }
 
-    return tally;
-  }
-
-  private static long PartTwo()
-  {
-    long tally = 0;
-
-    return tally;
-  }
-
-  private static string[] GetData(string[] args)
-  {
-    var filename = "sample.txt";
-    if (args.Length > 0 && !string.IsNullOrWhiteSpace(args[0]))
-      filename = args[0];
-
-    using var streamReader = new StreamReader(filename);
+    using var streamReader = new StreamReader(filePath);
     var data = streamReader.ReadToEnd().Split('\n', StringSplitOptions.RemoveEmptyEntries);
 
     return data;
   }
 
-
-  private static void PrintTitle(){
-    Console.WriteLine("# Advent of Code 2019 #");
-    Console.WriteLine("## Day 1: The Tyranny of the Rocket Equation ##");
-    Console.WriteLine("https://adventofcode.com/2019/day/1");
-    Console.WriteLine();
-  }
-
-  private static void PrintResult(string partNo, string result, Stopwatch sw)
-  {
+  private static void PrintResult(string partNo, string result, Stopwatch sw) {
     sw.Stop();
-    Console.WriteLine($"Part {partNo}\\");
-    Console.WriteLine($"Result: {result}\\");
-    Console.WriteLine($"Time elapsed (ms): {sw.Elapsed.TotalMilliseconds}");
-    Console.WriteLine();
+    Console.WriteLine($"Part {partNo} Result: {result} in {sw.Elapsed.TotalMilliseconds}ms");
     sw.Restart();
   }
 }
---
+++ AocTemplate/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace MyProject;
+
+internal static partial class Program {
+  private static long PartOne(string[] data) {
+    long tally = 0;
+
+    return tally;
+  }
+
+  private static long PartTwo(string[] data) {
+    long tally = 0;
+
+    return tally;
+  }
+}
